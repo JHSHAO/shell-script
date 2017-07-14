07e1305a71134e93f4826b3f42ab98bdeb2e7d5f
wangxuan <wangxuan19911119@163.com>
2017-07-14 11:17:13 +0800

    磁盘检查添加剩余可用磁盘大小

--- disk_check.sh
+++ disk_check.sh
@@ -11,24 +11,26 @@ do
 	hosts_ip=`echo $hosts_disk | awk '{print $1}'`
 	hosts_name=`echo $hosts_disk | awk '{print $3}'`
 	disk_v01=`sudo ssh -n $hosts_ip "df -TH" | awk '$7=="/data/v01"{print $6}' | sed 's/%//'`
+	disk_v01_avail=`sudo ssh -n $hosts_ip "df -TH" | awk '$7=="/data/v01"{print $5}'`
 	disk_v02=`sudo ssh -n $hosts_ip "df -TH" | awk '$7=="/data/v02"{print $6}' | sed 's/%//'`
-	echo "******************************************************" >> $path/disk_status
+	disk_v02_avail=`sudo ssh -n $hosts_ip "df -TH" | awk '$7=="/data/v02"{print $5}'`
+	echo "*****************************************************************" >> $path/disk_status
 	echo "${hosts_name}" >> $path/disk_status
 	if [ $disk_v01 -ge 80 -o $disk_v02 -ge 80 ]; then
 		echo "******************************************************" 
 		echo "${hosts_name}" 
 	fi
 	if [ $disk_v01 -ge 80 ]; then
-		echo -en "\033[31m/data/v01:${disk_v01}%\033[0m" >> $path/disk_status
-		echo -e "\033[31m/data/v01:${disk_v01}%\033[0m" 
+		echo -en "\033[31m/data/v01\t${disk_v01}%\t${disk_v01_avail}\033[0m" >> $path/disk_status
+		echo -e "\033[31m/data/v01\t${disk_v01}%\t${disk_v01_avail}\033[0m" 
 	else	
-		echo -en "/data/v01:${disk_v01}%" >> $path/disk_status
+		echo -en "/data/v01\t${disk_v01}%\t${disk_v01_avail}" >> $path/disk_status
 	fi
 
 	if [ $disk_v02 -ge 80 ]; then
-		echo -e "\033[31m\t/data/v02:${disk_v02}%\033[0m" >> $path/disk_status
-		echo -e "\033[31m\t/data/v02:${disk_v02}%\033[0m"
+		echo -e "\033[31m\t/data/v02\t${disk_v02}%\t${disk_v02_avail}\033[0m" >> $path/disk_status
+		echo -e "\033[31m/data/v02\t${disk_v02}%\t${disk_v02_avail}\033[0m"
 	else
-		echo -e "\t/data/v02:${disk_v02}%" >> $path/disk_status
+		echo -e "\t/data/v02\t${disk_v02}%\t${disk_v02_avail}" >> $path/disk_status
 	fi
 done < $hosts_file
