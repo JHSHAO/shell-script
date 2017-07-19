4b3c5bd7369d61d98181682f8e992bdd6ae43043
wangxuan <wangxuan19911119@163.com>
2017-07-19 12:21:32 +0800

    清理ogg日志脚本添加排除当前进程的日志逻辑

--- clean_ogg_log_bak.sh
+++ clean_ogg_log_bak.sh
@@ -10,31 +10,14 @@ for file in $ogg_del_log; do
 	echo "$file" >> $ogg_user_path/ogg_clean_file.log
 #	rm $file
 done
-#for file in `ls $ogg_root_path/dirrpt`; do
-#	echo "$ogg_root_path/$file" >> $ogg_user_path/ogg_clean_file.log
-#done
-#ogg进程信息
-#ogg_process=`$ogg_root_path/ggsci << EOF
-#info all
-#quit	
-#EOF`
-
-#echo "$ogg_process" | grep "EXTRACT" | awk '{print $3}' > $ogg_user_path/ogg_process.log
-
-#清理非当天的日志
-#while read file
-#do
-#	echo $file
-#	rm $file
-#done < $ogg_user_path/ogg_clean_file.log
 echo "完成删除日志！"
 #磁盘空间大于80%，清理当天日志
 ogg_disk=$(df -h | awk '$6=="/data/v01"{print $5}' | sed 's/%//g')
-if [ $ogg_disk -ge 80 ]; then
+if [ $ogg_disk -le 80 ]; then
 	echo "磁盘大小：$ogg_disk"
-	for file in `ls $ogg_root_path/dirrpt`; do
-		echo $file
+#	for file in `ls $ogg_root_path/dirrpt`; do
+#		echo $file >/dev/null 2>&1
 #		echo > $file
-	done
+#	done
 	echo "磁盘空间超过80%，清空日志！"
 fi
--- clean_ogg_log_task.sh
+++ clean_ogg_log_task.sh
@@ -8,10 +8,9 @@ ogg_user_path="/home/ogg"
 
 while read remotehost
 do
-#	if sudo ssh $remotehost test ! -e $ogg_user_path/clean_ogg_log.sh; then
-		sudo scp $path/clean_ogg_log.sh $remotehost:$ogg_user_path >/dev/null 2>&1
-#	fi
-	sudo ssh -tt $remotehost >/dev/null 2>&1 << EOF
+	sudo scp $path/clean_ogg_log.sh $remotehost:$ogg_user_path >/dev/null 2>&1
+	sudo ssh -tt $remotehost << EOF
+#清理ogg的dirrpt目录日志
 chmod 755  $ogg_user_path/clean_ogg_log.sh
 $ogg_user_path/clean_ogg_log.sh $ogg_root_path $ogg_user_path
 exit
