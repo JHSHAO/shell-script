970e05a0e62c27bc7f3943d52ca7ee29cb648ec2
wangxuan <wangxuan19911119@163.com>
2017-08-03 17:30:54 +0800

    监控磁盘10分钟发一次短信，在Linux的crontab里面配置

--- remote_host_monitor_task.sh
+++ remote_host_monitor_task.sh
@@ -5,7 +5,7 @@ hostsfile="$path/hosts_remote.txt"
 
 while read remotehost
 do
-    echo "******************************$remotehost***********************************"
+#    echo "******************************$remotehost***********************************"
     #短信内容
     content=""
     #磁盘使用率
@@ -13,7 +13,7 @@ do
     v01_arail=`sudo ssh -n $remotehost "df -TH" | grep v01 | awk '{print $5}'`
     v02=`sudo ssh -n $remotehost "df -TH"  | grep v02| awk '{print $6}' | tr -cd "[0-9]"`
     v02_arail=`sudo ssh -n $remotehost "df -TH"  | grep v02| awk '{print $5}'`
-    if [ $v01 -ge 80 ]; then
+    if [ $v01 -ge 90 ]; then
         content+="${remotehost}的/data/v01可用${v01_arail},使用百分比${v01}%。"
         large_files=`sudo ssh -n $remotehost du --exclude="/data/v01/ProvincesDatas/*" --max-depth=3 /data/v01/ | sort -n | tail -n 8 | awk '{print $2}'`
         for large_file in $large_files; do
@@ -22,7 +22,7 @@ do
         done
     fi
 
-    if [ $v02 -ge 80 ]; then
+    if [ $v02 -ge 90 ]; then
         content+="${remotehost}的/data/v02可用${v02_arail},使用百分比${v02}%。"
         large_files=`sudo ssh -n $remotehost du --exclude="/data/v02/ProvincesDatas/*" --max-depth=3 /data/v02/ | sort -n | tail -n 8 | awk '{print $2}'`
         for large_file in $large_files; do
@@ -51,7 +51,7 @@ do
     #发送短信
     interface_addr="http://10.161.11.182:8082/monitor/rest/message/sendMessage"
     content_type="Content-Type:application/json"
-    #recivers="13120228321,17600908312,13001927192,17600196269,15510798997"
+#    recivers="13120228321,17600908312,13001927192,17600196269,15510798997"
     recivers="17600908312"
     if [ ! -z "$content" ]; then
         curl $interface_addr -H $content_type -d "{\"recivers\":\"$recivers\",  \"content\": \"$content\"}"
