efb9427ee87fb8ae3e7faf105b3a53a7d753af36
wangxuan <wangxuan19911119@163.com>
2017-08-23 10:47:12 +0800

    删除ogg日志脚本用数组方法

--- clean_ogg_log_task_arr.sh
+++ clean_ogg_log_task_arr.sh
@@ -12,8 +12,8 @@ do
         scp $path/clean_ogg_log.sh $remotehost:$ogg_user_path >/dev/null 2>&1
         ssh -tt $remotehost << EOF
 #清理ogg的dirrpt目录日志
-chmod 755  $ogg_user_path/clean_ogg_log.sh
-$ogg_user_path/clean_ogg_log.sh $ogg_user_path
+chmod 755  $ogg_user_path/clean_ogg_log_arr.sh
+$ogg_user_path/clean_ogg_log_arr.sh $ogg_user_path
 exit
 EOF
     fi
