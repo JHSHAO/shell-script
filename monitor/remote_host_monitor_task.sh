119cf4c2fdc8fe7d92d48f96017bdf1aeffcbf6a
wangxuan <wangxuan19911119@163.com>
2017-08-23 11:15:35 +0800

    系统监控脚本

---
+++ monitor/remote_host_monitor_task.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+path=`pwd`
+hostsfile="$path/hosts_remote.txt"
+
+while read remotehost
+do
+    echo "******************************$remotehost***********************************"
+    #短信内容
+    content=""
+    #判断主机是否正常，即能否ssh链接。
+    sudo ssh -n $remotehost "df -TH" > /dev/null 2>&1
+    if [ $? -ne 0 ]; then
+        content+="${remotehost}主机不能ssh链接，可能宕机。"
+    fi
+    #磁盘使用率
+    v01=`sudo ssh -n $remotehost "df -TH" | grep v01 | awk '{print $6}' | tr -cd "[0-9]"`
+    v01_arail=`sudo ssh -n $remotehost "df -TH" | grep v01 | awk '{print $5}'`
+    v02=`sudo ssh -n $remotehost "df -TH"  | grep v02| awk '{print $6}' | tr -cd "[0-9]"`
+    v02_arail=`sudo ssh -n $remotehost "df -TH"  | grep v02| awk '{print $5}'`
+    if [ "$remotehost" == "inf13" ]; then
+        v01=`sudo ssh -n $remotehost "df -TH" | grep "/dev/sda3" | awk '{print $6}' | tr -cd "[0-9]"`
+        v01_arail=`sudo ssh -n $remotehost "df -TH" | grep "/dev/sda3" | awk '{print $5}'`
+        v02=`sudo ssh -n $remotehost "df -TH"  | grep "/dev/sda3" | awk '{print $6}' | tr -cd "[0-9]"`
+        v02_arail=`sudo ssh -n $remotehost "df -TH"  | grep "/dev/sda3" | awk '{print $5}'`     
+    fi
+    if [ $v01 -ge 90 ]; then
+        content+="${remotehost}的/data/v01可用${v01_arail},使用百分比${v01}%。"
+        large_files=`sudo ssh -n $remotehost du --exclude="/data/v01/ProvincesDatas/*" --max-depth=3 /data/v01/ | sort -n | tail -n 8 | awk '{print $2}'`
+        for large_file in $large_files; do
+            large_file_size=`sudo ssh -n $remotehost du -h --exclude="/data/v01/ProvincesDatas/*" --max-depth=0 $large_file | awk '{print $1"="$2}'`
+            content+="$large_file_size。"
+        done
+    fi
+
+    if [ $v02 -ge 90 ]; then
+        content+="${remotehost}的/data/v02可用${v02_arail},使用百分比${v02}%。"
+        large_files=`sudo ssh -n $remotehost du --exclude="/data/v02/ProvincesDatas/*" --max-depth=3 /data/v02/ | sort -n | tail -n 8 | awk '{print $2}'`
+        for large_file in $large_files; do
+            large_file_size=`sudo ssh -n $remotehost du -h --exclude="/data/v02/ProvincesDatas/*" --max-depth=0 $large_file | awk '{print $1"="$2}'`
+            content+="$large_file_size。"
+        done
+    fi
+    #网络连接数
+#   netnumarr=`sudo ssh -n $remotehost netstat -an|grep 6667|awk '{print $5}'|awk -F: '{print $1}'|sort -r|uniq -c|sort -nr|awk '{print$1}'`
+#   netcon=0
+#   for num in $netnumarr; do
+#       netcon=`expr $netcon + $num`
+#   done
+    netcon=`sudo ssh -n $remotehost netstat -an|grep 6667|wc -l`
+    if [ $netcon -ge 10000 ]; then
+        content+="${remotehost}网络连接数报警:${netcon}。"
+    fi
+    #负载
+    load_ave=`sudo ssh -n $remotehost uptime`
+    load_ave_1=`sudo ssh -n $remotehost uptime | sed "s/.*average://g" | awk -F , '{print $1}'`
+    load_ave_5=`sudo ssh -n $remotehost uptime | sed "s/.*average://g" | awk -F , '{print $2}'`
+    load_ave_15=`sudo ssh -n $remotehost uptime | sed "s/.*average://g" | awk -F , '{print $3}'`
+    if [ `echo load_ave_1\>=10|bc` -eq 1 -o `echo $load_ave_5\>=10|bc` -eq 1 -o `echo $load_ave_15\>=10|bc` -eq 1 ]; then
+        content+="${remotehost}负载报警:${load_ave}。"
+    fi
+    #发送短信
+    interface_addr="http://10.161.11.182:8082/monitor/rest/message/sendMessage"
+    content_type="Content-Type:application/json"
+    #recivers="13120228321,17600908312,13001927192,17600196269,15510798997"
+    recivers="17600908312"
+    if [ ! -z "$content" ]; then
+        curl $interface_addr -H $content_type -d "{\"recivers\":\"$recivers\",  \"content\": \"$content\"}"
+        send_date_name=`date +"%Y_%m_%d"`
+        send_time=`date +"%Y-%m-%d %H:%M:%S"`
+        find $path -name "sms_message_*.log" -type f -mtime +7 -exec rm {} \;
+        echo "{\"sendTime\":\"${send_time}\",\"content\":\"${content}\"}" >> $path/sms_message_${send_date_name}.log
+    fi
+done < $hostsfile
---
+++ monitor/sysanly.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+cd "$(dirname "$0")"
+path=`pwd`
+hostsfile="$path/hosts.txt"
+
+while read remotehost
+do
+	echo "[`date`] $remotehost system status >>>>>>>>>>>>"
+	logPath=dkmonitor/${remotehost}_$(date +'%Y-%m-%d')
+	echo "logPath:$logPath"
+	if [ ! -d "$logPath"  ]; then
+		mkdir -p "$logPath"
+	fi
+
+	echo "[`date`]===================system  diskinfo begin============" >> $logPath/diskinfo
+	sudo ssh -n $remotehost "df -TH" >> $logPath/diskinfo	
+	echo "[`date`]=================== system diskinfo end ============" >> $logPath/diskinfo
+
+	echo "[`date`]===================system uptime begin============" >> $logPath/uptime
+	sudo ssh -n $remotehost "uptime" >> $logPath/uptime
+	echo "[`date`]=================== system uptime end ============" >> $logPath/uptime
+	
+	echo "[`date`]===================system vmstat begin============" >> $logPath/vmstat
+	sudo ssh -n $remotehost "vmstat" >> $logPath/vmstat
+	echo "[`date`]=================== system vmstat end ============" >> $logPath/vmstat
+	
+	echo "[`date`]===================system mpstat begin============" >> $logPath/mpstat
+	sudo ssh -n $remotehost "mpstat -P ALL" >> $logPath/mpstat
+	echo "[`date`]=================== system mpstat end ============" >> $logPath/mpstat
+
+	echo "[`date`]===================system pidstat begin============" >> $logPath/pidstat
+	sudo ssh -n $remotehost "pidstat" >> $logPath/pidstat
+	echo "[`date`]=================== system pidstat end ============" >> $logPath/pidstat
+	
+	echo "[`date`]===================system iostat begin============" >> $logPath/iostat
+        sudo ssh -n $remotehost "iostat -x" >> $logPath/iostat
+        echo "[`date`]=================== system iostat end ============" >> $logPath/iostat
+
+	echo "[`date`]===================system free  begin============" >> $logPath/free
+        sudo ssh -n $remotehost "free –m" >> $logPath/free
+        echo "[`date`]=================== system free  end ============" >> $logPath/free
+
+	echo "[`date`]===================system sar  begin============" >> $logPath/sar
+        sudo ssh -n $remotehost "sar -n DEV 1 1" >> $logPath/sar
+        echo "[`date`]=================== system sar  end ============" >> $logPath/sar
+
+	echo "[`date`]===================system dmesg begin============" >> $logPath/dmesg
+        sudo ssh -n $remotehost "dmesg | tail -n 100" >> $logPath/dmesg
+        echo "[`date`]=================== system dmesg end ============" >> $logPath/dmesg
+	
+	echo "[`date`] connect to  $remotehost at port 6667 , info:" >> $logPath/netconect
+	sudo ssh -n $remotehost "netstat -an |grep 6667 | awk '{print \$5}' | awk -F: '{print \$1}' | sort -r | uniq -c  | sort -nr  " >> $logPath/netconect
+	echo "scan port 6667 @$remotehost finish!" >> $logPath/netconect
+
+	echo "[`date`] $remotehost system status <<<<<<<<<<<<"
+done < $hostsfile
+echo "finish port scan!"
