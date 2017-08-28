e3cfb670254f9c8f3ea66350a83a4854953a8895
wangxuan <wangxuan19911119@163.com>
2017-08-28 16:08:52 +0800

    将一个字符串按移动长度平分成几个字符串存储在数组中输出

---
+++ common/segment.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#分割字符串
+str=$1
+step=$2
+
+if [ -z "$step" ]; then
+    step=120
+fi
+
+if [ ! -z "$str" ]; then
+    len=${#str}
+    num=$[$len/$step]
+    if [ $[$len%$step] -ne 0 ]; then
+        num+=1
+    fi
+    
+    for ((i=0;i<$num;i++))
+    do
+       #echo ${str:$[$step*$i]:$step} 
+       str_arr[$i]=${str:$[$step*$i]:$step}
+    done
+    echo ${str_arr[@]}
+fi
