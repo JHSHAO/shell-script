9fdcc1f3263a723f9d1971599c33175afb9bc36d
wangxuan <wangxuan19911119@163.com>
2017-08-30 17:18:18 +0800

    发送短信独立模块，注释记录发送短信的日志

--- common/send_sms.sh
+++ common/send_sms.sh
@@ -24,9 +24,9 @@ if [ ! -z "$sms_content" ]; then
     do
        sms_content_segment=${sms_content:$[$step*$i]:$step}
        curl $interface_addr -H $content_type -d "{\"recivers\":\"$recivers\",  \"content\": \"$sms_content_segment\"}" > /dev/null 2>&1
-       send_date_name=`date +"%Y_%m_%d"`
-       send_time=`date +"%Y-%m-%d %H:%M:%S"`
-       find $path -name "sms_message_*.log" -type f -mtime +7 -exec rm {} \;
-       echo "{\"sendTime\":\"${send_time}\",\"smsContent\":\"${sms_content_segment}\"}" >> $path/sms_message_${send_date_name}.log
+       #send_date_name=`date +"%Y_%m_%d"`
+       #send_time=`date +"%Y-%m-%d %H:%M:%S"`
+       #find $path -name "sms_message_*.log" -type f -mtime +7 -exec rm {} \;
+       #echo "{\"sendTime\":\"${send_time}\",\"smsContent\":\"${sms_content_segment}\"}" >> $path/sms_message_${send_date_name}.log
     done
 fi
